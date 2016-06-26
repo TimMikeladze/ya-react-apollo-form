2f74031ec859162073a99c04746e10ffa76f8c43
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-06-26 01:09:42 -0700

    saving work

--- src/ApolloForm.js
+++ src/ApolloForm.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Form } from 'ya-react-redux-form';
+import { Form, FormHandler } from 'ya-react-redux-form';
+import _ from 'lodash';
 
 class ApolloForm extends Form {
   constructor(props, context) {
     super(props, context);
-    this.submit = this.submit.bind(this);
-  }
-  submit() {
+
+    this.handler.setMethod(({ name, form, dispatch, getState }) => {
+      if (props.mutation) {
+        // TODO handle multiple paramaters
+        const result = props.mutation(_.values(form)[0]);
+        return result;
+      }
+    });
+
+    this.handler.setOnSuccess((err, { name, form, dispatch, getState }) => {
+      console.log('Success');
+    });
+
+    this.handler.setOnFailure((err, { name, form, dispatch, getState }) => {
+      console.log('Failure');
+    });
   }
 }
 
